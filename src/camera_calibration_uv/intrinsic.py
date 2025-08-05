38edcaebc123b33a7933e62e7b7d2a95464becc1
Simon Thimm <2023463@stud.hs-mannheim.de>
2025-08-05 14:03:55 +0200

    intrinsic calibration

--- src/camera_calibration_uv/intrinsic.py
+++ src/camera_calibration_uv/intrinsic.py
@@ -3,7 +3,9 @@ import numpy as np
 from .utils import read_yaml
 
 """
+References:  
 https://docs.opencv.org/4.x/da/d13/tutorial_aruco_calibration.html
+https://github.com/opencv/opencv/blob/4.x/samples/python/aruco_detect_board_charuco.py#L72
 """
 
 def create_board(yaml_path: str) -> cv2.aruco.CharucoBoard:
@@ -26,10 +28,9 @@ def calibrate_intrinsic(args):
     if not input_vid.isOpened(): 
         raise ValueError('Failed to open camera')
     
-    all_charuco_corners, all_charuco_ids = [], []
     all_img_points, all_obj_points = [], []
-
     frame_idx = -1
+
     while True: 
         frame_idx += 1
         ret, frame = input_vid.read() 
@@ -37,15 +38,20 @@ def calibrate_intrinsic(args):
             break
         if (frame_idx % args.step_size) != 0: 
             continue
-        image_cpy = np.copy(frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        charuco_corners, charuco_ids, marker_corners, marker_ids = detector.detectBoard(image_cpy)
+        charuco_corners, charuco_ids, marker_corners, marker_ids = detector.detectBoard(gray)
         if not (marker_ids is None) and len(marker_ids) > 0:
-            cv2.aruco.drawDetectedMarkers(image_cpy, marker_corners)
+            cv2.aruco.drawDetectedMarkers(gray, marker_corners)
         if not (charuco_ids is None) and len(charuco_ids) > 0:
-            cv2.aruco.drawDetectedCornersCharuco(image_cpy, charuco_corners, charuco_ids)
-            if len(charuco_ids) > 3: 
-
+            cv2.aruco.drawDetectedCornersCharuco(gray, charuco_corners, charuco_ids)
+            if len(charuco_ids) >= 4: 
+                try: 
+                    obj_points, img_points = board.matchImagePoints(charuco_corners, charuco_ids)
+                    all_img_points.append(img_points)
+                    all_obj_points.append(obj_points)
+                except cv2.error as e: 
+                    print(f"Point matching failed: {e}")
 
         # print("charuco_corners: ", charuco_corners
         #       , "charuco_ids: ", charuco_ids
@@ -58,9 +64,20 @@ def calibrate_intrinsic(args):
         #     )
         #     print("Matched points")
 
-        cv2.imshow("Camera", frame)
+        cv2.imshow("Camera", gray)
         if cv2.waitKey(1) & 0xFF == ord('q'): 
             break
 
-    # input_vid.release() 
-    # cv2.destroyAllWindows() 
+    input_vid.release() 
+    cv2.destroyAllWindows() 
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.0001)
+    ret, cam_mtx, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(
+        all_obj_points, 
+        all_img_points, 
+        gray.shape[::-1],
+        cameraMatrix=None, 
+        distCoeffs=None, 
+        criteria=criteria,
+    )
+    print(ret)
